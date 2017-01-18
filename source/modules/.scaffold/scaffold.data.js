394f3947bc17ec4e2a455eee535ab4eb97ffae2e
Sebastian Weyrauch <git@dev-lab.de>
2017-01-18 10:37:26 +0100

    converted the .data.js file to ES6

--- source/modules/.scaffold/scaffold.data.js
+++ source/modules/.scaffold/scaffold.data.js
@@ -1,18 +1,15 @@
-'use strict';
-
-var _ = require('lodash'),
-	requireNew = require('require-new'),
+const requireNew = require('require-new'),
 	dataHelper = require('../../../helpers/data.js'),
 	defaultData = requireNew('../../data/default.data.js');
 
-var data = _.merge(defaultData, {
-		meta: {
-			title: '{{originalName}}',
-			className: '{{className}}',
-			keyName: '{{keyName}}',
-			code: dataHelper.getTemplateCode('{{name}}.hbs'),
-			documentation: dataHelper.getDocumentation('{{name}}.md')
-		}
-	});
+const data = Object.assign(defaultData, {
+	meta: {
+		title: '{{originalName}}',
+		className: '{{className}}',
+		keyName: '{{keyName}}',
+		code: dataHelper.getTemplateCode('{{name}}.hbs'),
+		documentation: dataHelper.getDocumentation('{{name}}.md')
+	}
+});
 
 module.exports = data;
